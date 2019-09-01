e3af2f05603e0945a2dbe0b70d812a65d819472b
谬老汉 <muimoon@gmail.com>
2019-09-01 14:36:34 +0800

    updated

--- esp8266/Blynk/Blynk.ino
+++ esp8266/Blynk/Blynk.ino
@@ -6,10 +6,11 @@ char auth [] = "67a17b1eafea4f828f444b4298867d05";
 void setup ()
 {
   Serial.begin (9600 );
-  Blynk.begin (auth , "Tenda_06C908", "Mykiso123");
+  //Blynk.begin (auth , "Tenda_06C908", "Mykiso123");
+  //Blynk.begin (auth , "Bunny_house", "asdfghjkl");
+  Blynk.begin (auth , "ChinaNet-hcr7", "nkdaspqr");
 }
 void loop ()
 {
   Blynk.run ();
 }
-
